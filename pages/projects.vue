02011344e12817524d48b1de9076eb948c4b8312
faisal <faisalshaikh8433@gmail.com>
2020-03-06 01:05:22 +0530

    updates project page contents

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,63 +1,254 @@
 <template>
-  <div class="w-full lg:w-3/4  rounded overflow-hidden shadow-lg ">
-    <div class="px-6 py-4 ">
-      <div class="font-bold text-xl mb-2 text-center">
-        Some of my learning projects.
-      </div>
-      <p class="text-gray-800 m-1 p-1 text-base">
+  <div class="w-full lg:w-3/4 rounded overflow-hidden shadow-lg">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2 text-center">Some of the projects I worked on.</div>
+      <div class="text-gray-800 m-1 p-1 text-base">
         <ul class="list-disc px-2">
           <li class="m-1 p-1">
-            <a href="https://rails-vue-sidenav.herokuapp.com" target="_blank" class="underline hover:bg-gray-200">Rails Vue (SPA)</a> 
+            <strong class="underline text-lg">Masterchef</strong>
+            <div class="p-2 italic">
+              <p class="p-1">
+                <span class="font-semibold">Description:</span>
+                A software for managing production and sales of cakes, sweets and chocolates.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Used:</span>
+                Ruby on Rails, Postgresql and materialized view.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Role:</span>
+                Build data entry forms, optimized software for handling large data, apply search on the records and exporting them in an excel.
+              </p>
+            </div>
           </li>
           <li class="m-1 p-1">
-            <a href="https://github.com/faisalshaikh8433/Todo-App-Vue#used-vuex-vue-router-and-persisted-state-in-local-storage" target="_blank" class="underline hover:bg-gray-200">Vue Todo App (Used Vuex, Vue Router and Persisted State in local storage)
-            </a>
+            <strong class="underline text-lg">Flowboard</strong>
+            <div class="p-2 italic">
+              <p class="p-1">
+                <span class="font-semibold">Description:</span>
+                A talent or artist management software for agencies.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Used:</span>
+                Ruby on Rails, Vuejs and Postgresql.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Role:</span>
+                Build data entry forms, optimized search form and refactored ui.
+              </p>
+            </div>
+          </li>
+          <li class="m-1 p-1">
+            <strong class="underline text-lg">Galactus</strong>
+            <div class="p-2 italic">
+              <p class="p-1">
+                <span class="font-semibold">Description:</span>
+                A laravel app which provides api for authentication, placing orders, creating & finding customers.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Used:</span>
+                Laravel & postgresql as a database.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Role:</span>
+                Api's written by me.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">GitUrl:</span>
+                <a
+                  target="_blank"
+                  href=" https://github.com/faisalshaikh8433/galactus_laravel"
+                  class="hover:text-blue-600"
+                >https://github.com/faisalshaikh8433/galactus_laravel</a>
+              </p>
+            </div>
+          </li>
+          <li class="m-1 p-1">
+            <strong class="underline text-lg">Memorizer</strong>
+            <div class="p-2 italic">
+              <p class="p-1">
+                <span class="font-semibold">Description:</span>
+                A laravel learning project of mine. This app provides user registration and lets them save an image with a description as a memory. Still working on it.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Used:</span>
+                Laravel, mysql as a database & bootstrap.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Url:</span>
+                <a
+                  target="_blank"
+                  href="https://memorizer-webapp.herokuapp.com"
+                  class="hover:text-blue-600"
+                >https://memorizer-webapp.herokuapp.com</a>
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">GitUrl:</span>
+                <a
+                  target="_blank"
+                  href="https://github.com/faisalshaikh8433/memorizer"
+                  class="hover:text-blue-600"
+                >https://github.com/faisalshaikh8433/memorizer(wip).</a>
+              </p>
+            </div>
+          </li>
+          <li class="m-1 p-1">
+            <strong class="underline text-lg">Kaam Easy</strong>
+            <div class="p-2 italic">
+              <p class="p-1">
+                <span class="font-semibold">Description:</span>
+                A website for booking services like ac repairing, installing, car mechanics or whatever service is available on the website. Technicians or workers are notified automatically on a real time basis according to the booking date.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Used:</span>
+                Php, javascript, jquery, html5 server sent events and mysql.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Url:</span>
+                <a
+                  target="_blank"
+                  href="https://kaameasy.com"
+                  class="hover:text-blue-600"
+                >https://kaameasy.com (wip or dead).</a>
+              </p>
+            </div>
+          </li>
+          <li class="m-1 p-1">
+            <strong class="underline text-lg">Rails with Vue</strong>
+            <div class="p-2 italic">
+              <p class="p-1">
+                <span class="font-semibold">Description:</span>
+                Learning project of mine. It is a SPA powered by vuejs.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Used:</span>
+                Vuejs, vue router, tailwind css, ruby on rails and postgresql.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Url:</span>
+                <a
+                  target="_blank"
+                  href="https://rails-vue-sidenav.herokuapp.com"
+                  class="hover:text-blue-600"
+                >https://rails-vue-sidenav.herokuapp.com</a>
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">GitUrl:</span>
+                <a
+                  target="_blank"
+                  href="https://github.com/faisalshaikh8433/Vue-Side-Navgation-layout"
+                  class="hover:text-blue-600"
+                >https://github.com/faisalshaikh8433/Vue-Side-Navgation-layout</a>
+              </p>
+            </div>
+          </li>
+          <li class="m-1 p-1">
+            <strong class="underline text-lg">React Spend Money App</strong>
+            <div class="p-2 italic">
+              <p class="p-1">
+                <span class="font-semibold">Description:</span>
+                A small fun project for spending money on several things.It is an Open source project, the site is built by our company.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Used:</span>
+                nextjs react framework & tailwind css.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Role:</span>
+                I contributed money spending logic & small design tweeks.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Url:</span>
+                <a
+                  target="_blank"
+                  href="https://spend-money.netlify.com"
+                  class="hover:text-blue-600"
+                >https://spend-money.netlify.com</a>
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">GitUrl:</span>
+                <a
+                  target="_blank"
+                  href="https://github.com/faisalshaikh8433/BudgetVisualizer"
+                  class="hover:text-blue-600"
+                >https://github.com/faisalshaikh8433/BudgetVisualizer</a>
+              </p>
+            </div>
+          </li>
+          <li class="m-1 p-1">
+            <strong class="underline text-lg">Vue Todo App</strong>
+            <div class="p-2 italic">
+              <p class="p-1">
+                <span class="font-semibold">Description:</span>
+                Learning project of mine. Add, edit, delete & mark complete a todo. Your todos will be saved locally.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Used:</span>
+                vuejs, vuex, vue router, persisted state on local storage and tailwind css.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Role:</span>
+                I contributed money spending logic & small design tweeks.
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">Url:</span>
+                <a
+                  target="_blank"
+                  href="https://simpletodosapp.netlify.com"
+                  class="hover:text-blue-600"
+                >https://simpletodosapp.netlify.com</a>
+              </p>
+              <p class="p-1">
+                <span class="font-semibold">GitUrl:</span>
+                <a
+                  target="_blank"
+                  href="https://github.com/faisalshaikh8433/Todo-App-Vue"
+                  class="hover:text-blue-600"
+                >https://github.com/faisalshaikh8433/Todo-App-Vue</a>
+              </p>
+            </div>
           </li>
         </ul>
-      </p>
-    </div>
-    <div class="px-6 py-4 ">
-      <div class="font-bold text-xl mb-2 text-center">
-        Some projects I worked on.
       </div>
-      <p class="text-gray-800 m-1 p-1 text-base">
-        <ul class="list-disc px-2">
-          <li class="m-1 p-1">
-            Artist Schedule Management Software For Agencies.
-          </li>
-          <li class="m-1 p-1">
-            Bakery Management system.
-          </li>
-          <li class="m-1 p-1">
-            School Bill Inventory.
-          </li>
-          <li class="m-1 p-1">
-            Air Condition Inventory for installation, service, and billing.
-          </li>
-          <li class="m-1 p-1">
-            Full Fledge Inventory System with production, sale, purchase, export, accounts.
-          </li>
-        </ul>
-      </p>
     </div>
     <div class="px-6 py-4">
-      <div class="text-center font-bold text-xl mb-2">
-        Blogs 
+      <div class="font-bold text-xl mb-2 text-center">Project types I worked on.</div>
+      <div class="text-gray-800 m-1 p-1 text-base">
+        <ul class="list-disc px-2">
+          <li class="m-1 p-1">Artist Schedule Management Software For Agencies.</li>
+          <li class="m-1 p-1">Bakery Management system.</li>
+          <li class="m-1 p-1">School Bill Inventory.</li>
+          <li class="m-1 p-1">Air Condition Inventory for installation, service, and billing.</li>
+          <li
+            class="m-1 p-1"
+          >Full Fledge Inventory System with production, sale, purchase, export, accounts.</li>
+        </ul>
       </div>
-      <p class="text-gray-800 m-1 p-1 text-base">
+    </div>
+    <div class="px-6 py-4">
+      <div class="text-center font-bold text-xl mb-2">Blogs</div>
+      <div class="text-gray-800 m-1 p-1 text-base">
         <ul class="list-disc px-2">
           <li class="m-1 p-1">
             <div>
-              <a href="https://dev.to/faisalshaikh8433/streaming-csv-files-in-ruby-on-rails-2oda" target="_blank" class="underline">Streaming CSV files in Ruby On Rails</a>
+              <a
+                target="_blank"
+                href="https://dev.to/faisalshaikh8433/streaming-csv-files-in-ruby-on-rails-2oda"
+                class="underline"
+              >Streaming CSV files in Ruby On Rails</a>
             </div>
           </li>
           <li class="m-1 p-1">
             <div>
-              <a href="https://dev.to/faisalshaikh8433/recognizing-text-from-an-image-using-ruby-2nj8" target="_blank" class="underline">Recognizing text from an image using Ruby</a>
+              <a
+                target="_blank"
+                href="https://dev.to/faisalshaikh8433/recognizing-text-from-an-image-using-ruby-2nj8"
+                class="underline"
+              >Recognizing text from an image using Ruby</a>
             </div>
           </li>
         </ul>
-      </p>
+      </div>
     </div>
   </div>
 </template>

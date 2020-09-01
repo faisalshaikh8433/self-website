b5cf73c37d111be04f06ca6cd003b56b3c1699a6
faisal <faisalshaikh8433@gmail.com>
2020-09-01 10:51:11 +0530

    adds target blank for dev button

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,26 +3,22 @@
     <a href="https://twitter.com/shaikfaisal_dev" target="_blank">
       <span
         class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-        >Twitter</span
-      >
+      >Twitter</span>
     </a>
     <a href="https://github.com/faisalshaikh8433" target="_blank">
       <span
         class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-        >GitHub</span
-      >
+      >GitHub</span>
     </a>
     <a href="https://www.linkedin.com/in/faisal-sk" target="_blank">
       <span
         class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-        >LinkedIn</span
-      >
+      >LinkedIn</span>
     </a>
-    <a href="https://dev.to/faisalshaikh8433">
+    <a href="https://dev.to/faisalshaikh8433" target="_blank">
       <span
         class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
-        >Dev</span
-      >
+      >Dev</span>
     </a>
   </div>
 </template>
